ee0f90e50a3156fd7d0076a3be6cf69304b29402
Kyle Holzinger <kylelholzinger@gmail.com>
2015-12-13 01:37:40 -0500

    [KH] Display previously played tracks in the chrome extension!

--- content.js
+++ content.js
@@ -1,10 +1,31 @@
 "use strict";
+chrome.storage.local.remove("tracks");
+chrome.storage.local.remove("SCHistory");
+
 
 var hist = [];
 
 var songObserver = new MutationObserver(
-    function () {
-        console.log("New song");
+    function (muts) {
+        var title;
+        for(var i = 0; i < muts.length; i++) {
+            if (muts[i].target.classList.contains("playbackSoundBadge")) {
+                var cur = muts[i].target.getElementsByClassName("playbackSoundBadge__title sc-truncate")[0];
+                title = cur.childNodes[1].innerHTML.slice(15);
+                break;
+            }
+        }
+        if (title !== undefined) {
+            var all;
+            chrome.storage.local.get("SCHistory", function (items) {
+                console.log(items);
+                if (items.SCHistory === undefined) items.SCHistory = [];
+
+                items.SCHistory.push(title);
+                chrome.storage.local.set({"SCHistory": items.SCHistory});
+                console.log("done");
+            });
+        }
     }
 );
 
--- popup.js
+++ popup.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var test = document.getElementsByClassName("playControls__soundBadge")[0];
-    alert(test);
+    var xs;
+    chrome.storage.local.get("SCHistory", function (items) {
+        xs = items.SCHistory;
+        if (xs) {
+            var hist = document.getElementById("history");
+            var parNode = document.createElement("div");
+            for (var i = 0; i < xs.length; i++) {
+                var item = document.createElement("div");
+                item.innerHTML = xs[i];
+                parNode.appendChild(item);
+            }
+            hist.appendChild(parNode);
+        }
+    });
 });
